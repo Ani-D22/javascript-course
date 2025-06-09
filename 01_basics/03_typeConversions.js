f4dcf2ed2afdd3c672a2d5ff97ef2168a92dd81a
Aniket Date <dateaniket07@gmail.com>
2025-06-09 18:47:25 +0530

    updated typeConversions and README.md

--- 01_basics/03_typeConversions.js
+++ 01_basics/03_typeConversions.js
@@ -30,3 +30,61 @@ let num = 312;
 let stringNum = String(num);
 console.log(stringNum);
 console.log(typeof stringNum);
+
+// =======================================
+
+let demo = 3
+let negativeDemo = -demo
+console.log(negativeDemo)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2%2);
+console.log(2*2);
+console.log(2**2);
+
+let str1 = "Hello";
+let str2 = " JavaScript";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + 2 + 2);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+/**
+ * In javascript, the comparison(>,<,>=,<=)
+ * and equality-check(==) operators behave differently
+ * the equality-check works as usual but
+ * the comparison operators convert a null value to a 0
+ * and then performs action on them.
+ */
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(undefined >= 0);
+
+
+/**
+ * A (===) strict check operator is used to strict check equality of operands.
+ * This differs from the (==) operator as it checks for the value
+ * as well as the data-type of them as well.
+ */
+
+console.log("2" === 2)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # javascript-course
 
 <h3>JS Standards Documentations:</h3>
+
 1. [text](https://tc39.es/ecma262/)
+
 2. [text](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
